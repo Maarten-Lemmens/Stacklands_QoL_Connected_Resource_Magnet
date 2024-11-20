040287281c51af6949c0f6b92a14fb1e9e62c517
bureauPlant <bureau_plant@mail.com>
2024-11-20 21:53:22 +0100

    v0.0.3
    QoL - New type of "Connected Resource Magnet" that has 1 output road connector.

--- Mod.cs
+++ Mod.cs
@@ -3,13 +3,49 @@ using System;
 using System.Collections;
 using UnityEngine;
 
-namespace ExampleModNS
+namespace ConnectedResourceMagnetModNS
 {
-    public class ExampleMod : Mod
+    public class ConnectedResourceMagnetMod : Mod
     {
         public override void Ready()
         {
-            Logger.Log("Ready!");
+            WorldManager.instance.GameDataLoader.AddCardToSetCardBag(SetCardBagType.AdvancedBuildingIdea, "blueprint_connected_resource_magnet", 1);
         }
     }
+
+    public class ConnectedResourceMagnet : ResourceMagnet
+	{
+		public override void UpdateCard()
+		{
+			base.UpdateCard();
+            if (string.IsNullOrEmpty(PullCardId))
+			{
+				nameOverride = SokLoc.Translate("card_connected_resource_magnet_name");
+			}
+			if (MyGameCard.HasChild) {
+				if (MyGameCard.CardConnectorChildren.Any((CardConnector x) => x.ConnectionType == ConnectionType.Transport && x.CardDirection == CardDirection.output && x.ConnectedNode != null)) {
+					MyGameCard.StartTimer(5f, CompleteTransfer, SokLoc.Translate("card_connected_resource_magnet_status"), GetActionId("CompleteTransfer"));
+				}
+			}
+			else
+			{
+				MyGameCard.CancelTimer(GetActionId("CompleteTransfer"));
+			}
+		}
+
+		public override bool CanHaveCardsWhileHasStatus()
+		{
+			return true;
+		}
+	
+		[TimedAction("complete_transfer")]
+		public void CompleteTransfer()
+		{
+			if (MyGameCard.CardConnectorChildren.Any((CardConnector x) => x.ConnectionType == ConnectionType.Transport && x.CardDirection == CardDirection.output && x.ConnectedNode != null)) {
+				GameCard child  = MyGameCard.GetLeafCard();
+			    child.RemoveFromParent();
+    			WorldManager.instance.TrySendWithPipe(child, MyGameCard, 0);
+            }        
+		}		
+	}
 }
